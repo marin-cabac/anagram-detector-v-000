0590c87a4a879cb839f748aec66200fc1e5656ab
Marin Cabac <marin.cabac@gmail.com>
2018-02-24 08:54:51 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,4 +2,15 @@
 
 
 class Anagram
+  attr_accessor :s
+  def initialize(s)
+    @s = s 
+    
+   end 
+   def match(r)
+    s = @s.split("").sort
+     
+     res =r.select{|x| x.split("").sort == s}
+     
+    
 end 
