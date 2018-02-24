5d282841ba0878557976599f5e977edd98650d79
Marin Cabac <marin.cabac@gmail.com>
2018-02-24 13:30:34 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+
+
+class Anagram
+  attr_accessor :s
+  def initialize(s)
+    @s = s
+
+   end
+   def match(r)
+    s_sort = @s.split("").sort
+      r.select{|x| x.split("").sort == s_sort}
+   end
+end
